cb15b63d676aa2c3e69fa194ad5b9d9ed5b89021
Pchemelil <85558913+Pchemelil@users.noreply.github.com>
2021-11-16 21:45:28 +0300

    Add files via upload

---
+++ doWhile(12-50).cpp
@@ -0,0 +1,20 @@
+//137339 Peter Chemelil
+//while loop exercise
+#include <iostream>
+using namespace std;
+
+int main(){
+	int number = 12;
+	
+	
+	do{
+	
+	cout<<  "\n"<<number;	
+	number = number + 2;
+	    
+	}while(number<=50);
+
+
+	
+	return 0;
+}
